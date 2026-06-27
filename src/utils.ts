03014a4f468dc5721273ffe05a07be8ab33facd7
Stefan Predolac <stefan.predolac@appsco.com>
2026-06-27 20:26:59 +0200

    first commit

---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+import type { ListItem } from "./types";
+
+export const generateMockData = (count: number): ListItem[] => {
+  return Array.from({ length: count }, (_, index) => ({
+    id: index + 1,
+    name: `Element ${index + 1}`,
+  }));
+};
